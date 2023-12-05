5489a8de5ea921d25aa323fb660e8522c1a9afc7
luke-herren11 <herrenl2023@gmail.com>
2023-12-05 16:59:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,32 @@
 """
 
 #returns dictionary
-def readCSV(file_name):
+#def readCSV(file_name):
     
 
 
 
 if __name__ == "__main__":
     
+    print("Hello! Welcome to our stock market advisor program. The goal of "
+          "this program is to help you choose what stock to invest your "
+          "money in. \n\nThere are two options, press 1 if you would like to "
+          "provide your own stock and data, or press 2 if you would like "
+          "the program to choose 5 of the best stocks from the S&P 500.")
     
+    #Input validation
+    while True:
+        try:
+            choice = int(input("Please enter 1 or 2: "))
+        
+            if(choice == 1):
+                 print("input")
+                 break
+            elif(choice == 2):
+                 print("csv")
+                 break
+            else:
+                print("Invalid input!")
+                 
+        except ValueError:
+            print("Invalid input!")
